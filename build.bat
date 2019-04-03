9bcc365902da9975d64e01d4bbda968725aa3c63
TBrown <2632056+tellybrown@users.noreply.github.com>
2019-04-02 22:35:57 -0500

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,14 @@
+@echo Off
+
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+ 
+set version=0.1.0
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+
+
+dotnet build ElasticCache.sln -c %config%
